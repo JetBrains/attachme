e52211ca1c70e99c290172b1a44ed4795c00e641
Samvel Abrahamyan <samvel1024@gmail.com>
2019-10-31 10:10:29 +0100

    Add agent args for port and python interpreter, implement busy waiting for debugger to attach in python, get rid of sudo requirement

--- agent/src/main/java/com/attachme/agent/Agent.java
+++ agent/src/main/java/com/attachme/agent/Agent.java
@@ -2,25 +2,62 @@ package com.attachme.agent;
 
 import java.io.*;
 import java.lang.instrument.Instrumentation;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class Agent {
 
-  public static void premain(String args, Instrumentation instrumentation) {
-    Timer t = new Timer();
-    t.schedule(new TimerTask() {
-      @Override
-      public void run() {
-        new DebugPortTask().run();
-      }
-    }, 500);
 
+  private static Map<String, String> parseArgs(String args) {
+    if (args == null || args.trim().isEmpty())
+      return Collections.emptyMap();
+    Set<String> allowed = new HashSet<>(Arrays.asList("port", "python"));
+    Map<String, String> ans = new HashMap<>();
+    for (String arg : args.split(",")) {
+      String kv[] = arg.split(":");
+      if (kv.length != 2) {
+        throw new IllegalArgumentException("Illegal argument format");
+      }
+      ans.put(kv[0], kv[1]);
+    }
+    if (ans.keySet().retainAll(allowed)) {
+      throw new IllegalArgumentException("Illegal argument format");
+    }
+    // Validate the values
+    try {
+      if (ans.containsKey("port"))
+        Integer.parseInt(ans.get("port"));
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("Illegal port number", e);
+    }
+    if (ans.containsKey("python") && !new File(ans.get("python")).exists()) {
+      throw new IllegalArgumentException("Python path does not exist");
+    }
+    return ans;
+  }
+
+  public static void premain(String strArgs, Instrumentation instrumentation) {
+    Map<String, String> args;
+    try {
+      args = parseArgs(strArgs);
+    } catch (IllegalArgumentException e) {
+      System.err.println("[attachme] FATAL ERROR: " + e.getMessage());
+      System.exit(1);
+      return;
+    }
+    // It's started from a thread to actively wait for jdwp to attach, it's required when suspend=y
+    Thread task = new Thread(new DebugPortTask(args), "AttachmeAgentTread");
+    task.setDaemon(true);
+    task.start();
   }
 
   private static class DebugPortTask implements Runnable {
+
+    final Map<String, String> args;
+
+    private DebugPortTask(Map<String, String> args) {
+      this.args = args;
+    }
+
     @Override
     public void run() {
       try {
@@ -46,15 +83,16 @@ public class Agent {
     }
 
     private void doRun() throws InterruptedException, IOException {
-      Process python = new ProcessBuilder()
-        .command("python3")
-        .redirectErrorStream(true)
-        .start();
+      ProcessBuilder builder = new ProcessBuilder()
+        .command(args.getOrDefault("python", "python"))
+        .redirectErrorStream(true);
+      builder.environment().put("ATTACHME_PORT", args.getOrDefault("port", "7857"));
+      Process python = builder.start();
       pipeScript(python);
       BufferedReader script = new BufferedReader(new InputStreamReader(python.getInputStream()));
       Scanner sc = new Scanner(script);
       while (sc.hasNext()) {
-        System.err.println("[python] " + sc.nextLine());
+        System.err.println("[attachme] " + sc.nextLine());
       }
       python.waitFor();
       if (python.exitValue() != 0) {
