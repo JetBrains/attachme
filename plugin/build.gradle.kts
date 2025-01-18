e510b9ad39716e049bb6467bd064be13e5e571e1
Justin Kaeser <justin.kaeser@jetbrains.com>
2025-01-18 10:50:25 +0100

    attempt to fix "toolchain download repositories have not been configured" on CI

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -55,8 +55,6 @@ tasks {
         }
         duplicatesStrategy = DuplicatesStrategy.INCLUDE
     }
+
 }
-
-
-
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
 rootProject.name = "attachme"
 include("agent")
 include("plugin")
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+}
